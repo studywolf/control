4e94cb9c10543a5f5e798c0ec2605893bb21c321
travis dewolf <travis.dewolf@gmail.com>
2021-03-18 14:45:37 -0400

    Fixed two_link setup.py import

--- studywolf_control/arms/two_link/setup.py
+++ studywolf_control/arms/two_link/setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
   
 setup(
   cmdclass = {'build_ext': build_ext},
   ext_modules=[Extension("py2LinkArm", 
                sources=["py2LinkArm.pyx"],
+               include_dirs=[np.get_include()],
                language="c++"),],
 )
 
